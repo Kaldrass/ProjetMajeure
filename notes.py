0cba91d7b50e190e17944ecb46bf663cf29a82c9
Jonathan Bouyer <63496507+Jo-Byr@users.noreply.github.com>
2022-06-02 11:07:32 +0200

    Lecture des croches

--- notes.py
+++ notes.py
@@ -13,25 +13,17 @@ def line_evaluator(rho,theta,x):
 
 def lecture(img):
     img = img[int(0.02*img.shape[0]):int(0.98*img.shape[0]) , int(0.02*img.shape[1]):int(0.98*img.shape[1])]
-    
+
     if (len(img.shape) == 3):
         ny,nx,nc = img.shape
         I = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-    
+
     I = cv2.adaptiveThreshold(I, 255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 31, 15)
     I = 255 - I
-    
-    d = int(0.9*I.shape[0]/168)
-    d2 = int(0.25*I.shape[0]/168)
-    SE = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (d,d))
-    SE2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (d2,d2))
-    
+
     d = int(I.shape[0]/168)
-    
-    I_f = cv2.morphologyEx(I, cv2.MORPH_CLOSE, SE2, iterations = 1)
-    
-    I_e = cv2.erode(I_f,SE)
-    
+
+
     #Positionnement des portées (Transformée de Hough)
     res = img
     lines = cv2.HoughLines(I,1,np.pi/1000,int(img.shape[0]/2.5))
@@ -43,32 +35,43 @@ def lecture(img):
     for l in lines:
         R.sort()
         for rho,theta in l:
-            treated = False
-            #On s'assure de ne pas avoir déjà une ligne similaire en mémoire
-            if len(R) > 0:
-                i = 0
-                while (i < len(R) and R[i] < rho + d):
-                    if abs(rho - R[i]) < 0.5*dr:
-                        treated = True
-                    i += 1
-            if not(treated):
-                R.append(rho)
-                T.append(theta)
-                nb += 1
-                a = np.cos(theta)
-                b = np.sin(theta)
-                x0 = a*rho
-                y0 = b*rho
-                x1 = int(x0 + s*(-b))
-                y1 = int(y0 + s*(a))
-                x2 = int(x0 - s*(-b))
-                y2 = int(y0 - s*(a))
-                cv2.line(res,(x1,y1),(x2,y2),(255,0,0),1)
-                
+            if rho > 0:
+                treated = False
+                #On s'assure de ne pas avoir déjà une ligne similaire en mémoire
+                if len(R) > 0:
+                    i = 0
+                    while (i < len(R) and R[i] < rho + d):
+                        if abs(rho - R[i]) < 0.75*dr:
+                            treated = True
+                        i += 1
+                if not(treated):
+                    R.append(rho)
+                    T.append(theta)
+                    nb += 1
+                    a = np.cos(theta)
+                    b = np.sin(theta)
+                    x0 = a*rho
+                    y0 = b*rho
+                    x1 = int(x0 + s*(-b))
+                    y1 = int(y0 + s*(a))
+                    x2 = int(x0 - s*(-b))
+                    y2 = int(y0 - s*(a))
+                    cv2.line(res,(x1,y1),(x2,y2),(255,0,0),1)
+
+    D = sum([R[k] for k in range(4,len(R),5)]) - sum([R[k] for k in range(0,len(R),5)])   
+    d = int(D/(len(R) - len(R)//5) ) 
+
     #Repérage des notes
+    d2 = int(d/4)
+    SE = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (int(0.95*d),int(0.95*d)))
+    SE2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (d2,d2))
+
+    I_f = cv2.morphologyEx(I, cv2.MORPH_CLOSE, SE2, iterations = 1)
+
+    I_e = cv2.erode(I_f,SE)
     notes = np.argwhere(I_e == 255) #Listes des coordonnées des "notes"
     notes_traitees = [] #Liste des positions des notes
-    
+
     for n in notes:
         treated = False
         #On vérifie si la note a déjà été traitée
@@ -87,17 +90,22 @@ def lecture(img):
             notes_traitees.append(B)
             
     res_notes = img
-    
+
     for b in notes_traitees:
-        res_notes[b[0]-5:b[0]+5,b[1]-5:b[1]+5,0] = 255
+        res_notes[b[0]-int(d/2):b[0]+int(d/2),b[1]-int(d/2):b[1]+int(d/2),0] = 255
         
     #Evaluation des notes
+    SE = np.ones((int(0.45*d),int(1.5*d)))
+    croches = cv2.morphologyEx(I,cv2.MORPH_OPEN,SE) #Ne garde que les barres croches
+
     tones = {}
+    rythme = {}
     droites = sorted(zip(R,T))
     for n in notes_traitees:
         x = n[1]
         y = n[0]
         
+        #Hauteur
         h = 0
         minimum = max(R) + 1
         
@@ -119,5 +127,16 @@ def lecture(img):
             h += 0.5
             
         tones[(y,x)] = h
+        
+        
+        #Rythme
+        
+        #On compte le nombre de pixels blancs dans le voisinage de la note sur l'image de croches
+        V = np.count_nonzero(croches[y-5*d:y+5*d , x-d:x+d]) 
+        if V > d**2/3:
+            rythme[(y,x)] = 0.5
+        else:
+            rythme[(y,x)] = 1.0
+        
     
-    return tones
+    return tones,rythme

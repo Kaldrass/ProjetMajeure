641921d5165e4ef6b6fa3534f676480fc300c589
Jonathan Bouyer <63496507+Jo-Byr@users.noreply.github.com>
2022-06-03 10:37:56 +0200

    Amélioration de la détection de hauteur des notes

--- notes.py
+++ notes.py
@@ -7,40 +7,47 @@ Created on Tue May 31 15:24:21 2022
 
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+from copy import deepcopy
 
 def line_evaluator(rho,theta,x):
-    return rho/np.sin(theta) - x*np.cos(theta)/np.sin(theta)
+    return (rho - x*np.cos(theta))/np.sin(theta)
 
-def lecture(img):
-    img = img[int(0.05*img.shape[0]):int(0.95*img.shape[0]) , int(0.05*img.shape[1]):int(0.95*img.shape[1])]
+def line_eval(x1,y1,x2,y2,x):
+    return (x-x2)/(x1-x2)*y1 + (x1-x)/(x1-x2)*y2
+
+def lecture(image):
+    img = deepcopy(image)
+    #img = img[100:2000,125:1530]
+    img = img[int(0.03*img.shape[0]):int(0.97*img.shape[0]) , int(0.03*img.shape[1]):int(0.97*img.shape[1])]
 
     if (len(img.shape) == 3):
         ny,nx,nc = img.shape
         I = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
 
-    I = cv2.adaptiveThreshold(I, 255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 31, 15)
-    I = 255 - I
-
     d = int(I.shape[0]/168)
+    I = cv2.adaptiveThreshold(I, 255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 2*int((2*d)//2)+1, 2*int(d//4)+1)
+    I = 255 - I
 
 
     #Positionnement des portées (Transformée de Hough)
     res = img
     lines = cv2.HoughLines(I,1,np.pi/1000,int(img.shape[0]/2.5))
-    s = np.sqrt(nx**2 + ny**2)
+    #s = np.sqrt(nx**2 + ny**2)
+    s = nx
     T = []
     R = []
     dr = d
     nb = 0
     for l in lines:
-        R.sort()
+        #R.sort()
         for rho,theta in l:
             if rho > 0:
                 treated = False
                 #On s'assure de ne pas avoir déjà une ligne similaire en mémoire
                 if len(R) > 0:
                     i = 0
-                    while (i < len(R) and R[i] < rho + d):
+                    while (i < len(R)):
                         if abs(rho - R[i]) < 0.75*dr:
                             treated = True
                         i += 1
@@ -56,19 +63,24 @@ def lecture(img):
                     y1 = int(y0 + s*(a))
                     x2 = int(x0 - s*(-b))
                     y2 = int(y0 - s*(a))
+                    
                     cv2.line(res,(x1,y1),(x2,y2),(255,0,0),1)
-
-    D = sum([R[k] for k in range(4,len(R),5)]) - sum([R[k] for k in range(0,len(R),5)])   
-    d = int(D/(len(R) - len(R)//5) ) 
+    
+    droites = sorted(zip(R,T))
+    
+    D = sum([droites[k][0] for k in range(4,len(droites),5)]) - sum([droites[k][0] for k in range(0,len(droites),5)])
+    d = int(D/(len(R) - len(R)//5))
+    #plt.imshow(res)
 
     #Repérage des notes
-    d2 = int(d/4)
+    d2 = int(d/5)
     SE = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (int(0.95*d),int(0.95*d)))
     SE2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (d2,d2))
 
     I_f = cv2.morphologyEx(I, cv2.MORPH_CLOSE, SE2, iterations = 1)
 
     I_e = cv2.erode(I_f,SE)
+    
     notes = np.argwhere(I_e == 255) #Listes des coordonnées des "notes"
     notes_traitees = [] #Liste des positions des notes
 
@@ -76,13 +88,13 @@ def lecture(img):
         treated = False
         #On vérifie si la note a déjà été traitée
         for k in notes_traitees:
-            if abs(k[0] - n[0]) < d and abs(k[1] - n[1]) < d:
+            if abs(k[0] - n[0]) < d/2 and abs(k[1] - n[1]) < d/2:
                 treated = True
                 break
             
         V = [] #Voisinage de la note
         for k in notes:
-            if abs(k[0] - n[0]) < d and abs(k[1] - n[1]) < d:
+            if abs(k[0] - n[0]) < d/2 and abs(k[1] - n[1]) < d/2:
                 V.append(k)
         #Calcul du barycentre du voisinage et ajout de ce dernier à la liste des notes traitées    
         if not(treated):
@@ -100,7 +112,7 @@ def lecture(img):
 
     tones = {}
     duration = {}
-    droites = sorted(zip(R,T))
+    
     for n in notes_traitees:
         x = n[1]
         y = n[0]
@@ -113,17 +125,17 @@ def lecture(img):
         #On recherche la droite la plus proche
         while i < len(droites) and droites[i][0] < y + 2*d:
             y_portee = line_evaluator(droites[i][0],droites[i][1],x)
-            
             if abs(y_portee - y) < abs(minimum):
                 minimum = y_portee - y
                 h = i
                 
             i += 1
         #Si l'écart à cette droite est trop grand, on ajoute/retire un demi-ton    
+        print((y,x),h,minimum)
         h = h%5
-        if minimum >= d/3:
+        if minimum >= d/4:
             h -= 0.5
-        elif minimum <= -d/3:
+        elif minimum <= -d/4:
             h += 0.5
             
         tones[(y,x)] = h
@@ -137,7 +149,7 @@ def lecture(img):
             duration[(y,x)] = 0.5
         else:
             duration[(y,x)] = 1.0
-    print(tones,duration)    
+    print(tones)        
     #Traitement
     trans = {-0.5:43 , 0:41 , 0.5:40 , 1:38 , 1.5:36 , 2:35 , 2.5:33 , 3:31 , 3.5:29 , 4:28 , 4.5:26}
     note = []
@@ -162,4 +174,7 @@ def lecture(img):
         rythme.append(duration[L[i][::-1]])
         note.append(trans[tones[L[i][::-1]]])
         
+    
+    plt.imshow(res,'gray')
+    
     return note,rythme
